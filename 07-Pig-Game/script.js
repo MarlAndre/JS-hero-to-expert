600120dc80d2633f94414f7cce5ea481ea5a4028
Marlene Andre <andre.marlene1990@gmail.com>
2022-07-12 18:47:03 -0400

    implemented switching player logic whendice 1 is rolled

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -5,6 +5,8 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 const diceEl = document.querySelector('.dice');
 // Select dice btns
@@ -18,6 +20,7 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -32,8 +35,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER FOR LOGIC WITH 2 PLAYERS
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
