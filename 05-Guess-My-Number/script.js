4b5638adc81817643d90020ad913e2b39681d734
Marlene Andre <andre.marlene1990@gmail.com>
2022-07-12 08:42:54 -0400

    changed background color when correct number is given

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -20,18 +20,37 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  // When no input
   if (!guess) {
     document.querySelector('.message').textContent =
       'Number must be between 1 and 20!';
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number !';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // Guess too high
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too high!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game.';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // Guess too low
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too low!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game.';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
